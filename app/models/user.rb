e59b8287c0a714714f11fd7b3f58397c398d8d27
Andreea Butca <cristina13andreea@gmail.com>
2023-03-05 16:36:34 +0200

    Include secure passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,11 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates(:email, presence: true, length: {maximum: 255},
             format: { with: VALID_EMAIL_REGEX }, uniqueness: true)
+  # A pair of virtual attributes (password and password_confirmation),
+  #   including presence validations upon object creation
+  #   and a validation requiring that they match
+  validates(:password, presence: true, length: {minimum: 6})
 
-  # has_secure_password
+  has_secure_password
 
 end
